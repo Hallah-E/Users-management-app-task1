a8362e68f66da4918b69a09ebd6fdc3d5de61820
Hallah-E <101795755+Hallah-E@users.noreply.github.com>
2022-08-19 22:30:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -19,14 +19,14 @@ app.get('/', (req, res)=>{
 
 //try to insert a user
 (async()=>{
-    //const jane= await db.model.User.create({first_name:'Layla', last_name: 'Vontaine'})
-    // console.log(jane)
-    const jane= await db.model.User.findOne({where:{id:4}})
-//     await jane.update({
+    //const user= await db.model.User.create({first_name:'Layla', last_name: 'Ahmed'})
+    // console.log(user)
+    const user= await db.model.User.findOne({where:{id:4}})
+//     await user.update({
 //         "address": "Khartoum, sudan",
 //         "gender": 1,
-//         "email": "halla.alhassan@gmail.com",
-//         "mobile": "+249127782507",
+//         "email": "********@gmail.com",
+//         "mobile": "+24*****782507",
 //         "job": "Backend",
 //         "salary": 5687
 //  })
